e531b601ba9c9a998c100eb6592919f935d64190
warren <warren.dodsworth@gmail.com>
2019-04-06 04:05:54 +1100

    ADD mailchimp signup popup slide in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+
 import { User } from '../models/user';
 import { AuthService } from './auth.service';
 import { FcmService } from './fcm.service';
@@ -11,16 +13,15 @@ import { FcmService } from './fcm.service';
 })
 export class AppComponent {
   title = 'WiseGuy Investor'
-
   user: User
   isCollapsed = true
-  adminMenu = false
-  currentMessage: any
+
 
   constructor(
     public auth: AuthService,
     public route: ActivatedRoute,
     public router: Router,
+    public http: HttpClient,
     public fcm: FcmService) { }
 
   ngOnInit() {
@@ -33,29 +34,46 @@ export class AppComponent {
       }
     })
 
-    this.fcm.currentMessage.subscribe(msg => {
-      this.currentMessage = msg
-    })
   }
 
-  testNotifyMe() {
-    this.fcm.testNotifyMe(this.user.uid)
-  }
-
-  loginFacebook() {
-    this.auth.loginFacebook()
-  }
   loginGoogle() {
     this.auth.loginGoogle()
   }
-
-  toggleAdminMenu() {
-    this.adminMenu = !this.adminMenu
-    localStorage.setItem('adminMenu', this.adminMenu.toString())
-  }
-  isView(name) {
-    var path = this.route.pathFromRoot.join('/')
-    path = path.substring(1, path.length).trim()
-    return path.indexOf(name) > -1;
-  }
 }
+
+
+ // this.fcm.currentMessage.subscribe(msg => {
+    //   this.currentMessage = msg
+    // })
+ // adminMenu = false
+  // currentMessage: any
+
+  // testNotifyMe() {
+  //   this.fcm.testNotifyMe(this.user.uid)
+  // }
+
+   // subscriber: any = {};
+  // subscribeToMailingList(user) {
+  //   var body = "FNAME=" + user.firstname + "&LNAME=" + user.lastname + "&EMAIL=" + user.email;
+
+  //   this.http.post("https://skaoss.us14.list-manage.com/subscribe/post?u=f87d6e1ec58cd04830f6a367b&amp;id=e58a2a6af0", body)
+  //     .subscribe((data) => {
+  //       console.log(data)
+  //       this.subscriber = {};
+  //     });
+  // }
+
+  // loginFacebook() {
+  //   this.auth.loginFacebook()
+  // }
+
+  // toggleAdminMenu() {
+  //   this.adminMenu = !this.adminMenu
+  //   localStorage.setItem('adminMenu', this.adminMenu.toString())
+  // }
+
+  // isView(name) {
+  //   var path = this.route.pathFromRoot.join('/')
+  //   path = path.substring(1, path.length).trim()
+  //   return path.indexOf(name) > -1;
+  // }
