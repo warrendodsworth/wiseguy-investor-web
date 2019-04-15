a0b0e938432a3f4d5dd8254504d3b5d903d0b800
warren <warren.dodsworth@gmail.com>
2019-04-16 09:28:39 +1000

    ADD channel page with playlist

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AboutComponent } from './about/about.component';
+import { ChannelComponent } from './channel/channel.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'channel', component: ChannelComponent },
 ];
 
 @NgModule({
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { BlogModule } from '../blog/blog.module';
 import { SharedModule } from '../shared/shared.module';
 import { AboutComponent } from './about/about.component';
+import { ChannelComponent } from './channel/channel.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [CommonModule, SharedModule, BlogModule, HomeRoutingModule],
-  declarations: [HomeComponent, AboutComponent],
+  declarations: [HomeComponent, AboutComponent, ChannelComponent],
 })
 export class HomeModule {}
