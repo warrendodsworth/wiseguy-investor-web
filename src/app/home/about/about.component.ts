f051f629c4222407a5c4d414adef592f01032989
Warren Dodsworth <warren.dodsworth@gmail.com>
2025-09-03 09:17:51 +1000

    refactor: remove unused SCSS file and clean up AboutComponent

--- src/app/home/about/about.component.ts
+++ src/app/home/about/about.component.ts
@@ -3,14 +3,10 @@ import { Component, OnInit } from '@angular/core';
 import { LayoutService } from '../../core/services/layout.service';
 
 @Component({
-  selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
   constructor(public layout: LayoutService) {}
 
-  ngOnInit() {
-    this.layout.disableContainer = true;
-  }
+  ngOnInit() {}
 }
